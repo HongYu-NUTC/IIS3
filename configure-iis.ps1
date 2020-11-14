31bf7012c59de90894cce926a76b807cf3ce3da9
HongYu-NUTC <73383021+HongYu-NUTC@users.noreply.github.com>
2020-11-14 11:50:29 +0800

    fix

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value "<html><body><h2>Welcome to Azure! My name is HongYu.</h2></body></html>"
+  -Value '<html><body><h2>Welcome to Azure! My name is Hong Yu.</h2><iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNTY0YTU1YWQtODg2Zi00NmE1LWFhNTAtOWJkMmYzYmVhODQxIiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>' 
